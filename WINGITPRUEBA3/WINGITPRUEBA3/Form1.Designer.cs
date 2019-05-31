e1fbb30e685ff2ca46182e3a4bb4d40ae8bd5096
JAVIER ANTON 2 <javidoc@v240jam.jazztel.es>
2019-05-31 12:45:11 +0200

    cambio 1 boton verde

--- WINGITPRUEBA3/WINGITPRUEBA3/Form1.Designer.cs
+++ WINGITPRUEBA3/WINGITPRUEBA3/Form1.Designer.cs
@@ -43,12 +43,13 @@
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.button2.Location = new System.Drawing.Point(466, 94);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(140, 99);
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
